3b6c7f69b8ea723635a04ab1e01c31f2c396195b
goliatone <burgosemi@gmail.com>
2019-08-03 21:28:40 -0700

    clean up example

--- example/server.js
+++ example/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const http = require('http');
+let logger = console;
 
 const server = http.createServer(function $createServer(req, res) {
     let crash = randomInt(0, 10) > 11;
@@ -20,25 +21,27 @@ server.on('connection', function $onConnection(socket) {
     const id = ++_connections._i;
     socket._connectionId = id;
     _connections[id] = socket;
-    console.log('child %s: add socket %s', process.pid, id);
+    logger.info('child %s: add socket %s', process.pid, id);
 
     socket.on('close', function() {
-        console.log('child %s: remove socket %s', process.pid, id);
+        logger.info('child %s: remove socket %s', process.pid, id);
         delete _connections[id];
     });
 });
 
 module.exports = {
-    start() {
-        console.log('------------- CHILD PROCESS ---------');
-        console.log('child %s: start listen on 3999', process.pid);
+    start(config) {
+        if (config.logger) logger = config.logger;
+
+        logger.info('------------- CHILD PROCESS ---------');
+        logger.info('child %s: start listen on 3999', process.pid);
         server.listen(3999);
     },
     close() {
         return new Promise(function $onCloseHandler(resolve) {
-            console.log('child %s: closing server...', process.pid);
+            logger.info('child %s: closing server...', process.pid);
             Object.keys(_connections).forEach(function $socRem(key) {
-                console.log('child %s: remove: %s', process.pid, key);
+                logger.info('child %s: remove: %s', process.pid, key);
                 const socket = _connections[key];
                 if (socket && socket.destroy) socket.destroy();
             });
