2c07210fb1850c237edbd767251a1a1f2737411d
goliatone <burgosemi@gmail.com>
2019-08-03 20:17:43 -0700

    add exampe

---
+++ example/cli.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const cluster = require('cluster');
+const ProcessManager = require('..');
+
+/**
+ * ! A) Improve: can we use cluster IPC send/message
+ * ! B) use busybody to handle socket in use
+ */
+if (cluster.isMaster) {
+    console.log('-------- LEADER PROCESS -------');
+    console.log(`leader: ${process.pid}`);
+    ProcessManager.leader.start({
+        instances: require('os').cpus().length
+    });
+} else {
+    const server = require('./server');
+    console.log('child: process', process.pid);
+
+    ProcessManager.worker.start({ shutdown: server.close }).then(function $ready() {
+        server.start();
+        process.send({ msg: `child ${process.pid}: listening on 3999` });
+    });
+}
+
+/*
+ProcessManager.wheRunningAsLeader(leader => {
+
+    leader.logger.info('-------- LEADER PROCESS -------');
+    leader.logger.info(`leader: ${process.pid}`);
+
+    leader.start({
+        instances: require('os').cpus().length
+    });
+});
+
+ProcessManager.whenRunningAsWorker(worker => {
+
+    const server = require('./server');
+
+    worker.logger.info('child: process', process.pid);
+
+    worker.start({
+        shutdown: server.close
+    }).then(_ => {
+        server.start();
+    });
+});
+*/
---
+++ example/server.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const http = require('http');
+
+const server = http.createServer(function $createServer(req, res) {
+    let crash = randomInt(0, 10) > 11;
+
+    if (crash) {
+        throw new Error('something here');
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Hello World! From ${process.pid} with love...\n`);
+});
+
+const _connections = { _i: 0 };
+
+//https://github.com/thedillonb/http-shutdown/blob/master/index.js
+server.on('connection', function $onConnection(socket) {
+    const id = ++_connections._i;
+    socket._connectionId = id;
+    _connections[id] = socket;
+    console.log('child %s: add socket %s', process.pid, id);
+
+    socket.on('close', function() {
+        console.log('child %s: remove socket %s', process.pid, id);
+        delete _connections[id];
+    });
+});
+
+module.exports = {
+    start() {
+        console.log('------------- CHILD PROCESS ---------');
+        console.log('child %s: start listen on 3999', process.pid);
+        server.listen(3999);
+    },
+    close() {
+        return new Promise(function $onCloseHandler(resolve) {
+            console.log('child %s: closing server...', process.pid);
+            Object.keys(_connections).forEach(function $socRem(key) {
+                console.log('child %s: remove: %s', process.pid, key);
+                const socket = _connections[key];
+                if (socket && socket.destroy) socket.destroy();
+            });
+
+            server.close(resolve);
+        });
+    }
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
