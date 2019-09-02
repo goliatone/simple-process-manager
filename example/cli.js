3f6438714b469971d9c2c86815369414de5a3002
goliatone <burgosemi@gmail.com>
2019-09-01 17:52:36 -0700

    update example

--- example/cli.js
+++ example/cli.js
@@ -2,15 +2,31 @@
 
 const cluster = require('cluster');
 const ProcessManager = require('..');
+const Metadata = require('..').metadata;
+const { resolve } = require('path');
 
 /**
  * ! A) Improve: can we use cluster IPC send/message
  * ! B) use busybody to handle socket in use
  */
 if (cluster.isMaster) {
-    console.log('-------- LEADER PROCESS -------');
+    console.log('############### LEADER PROCESS ###############');
     console.log(`leader: ${process.pid}`);
+
+    const metadata = new Metadata({
+        logger: console,
+    });
+
+    const info = {
+        name: 'example',
+        worker: resolve('./server'),
+        instances: require('os').cpus().length,
+        description: 'Example showing how to run CLI process manager'
+    };
+
     ProcessManager.leader.start({
+        info,
+        metadata,
         instances: require('os').cpus().length
     });
 } else {
